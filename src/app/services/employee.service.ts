5a70a9aaa7565541d0f22a90654958f3dae35f78
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-03-20 19:15:39 -0500

    Creacion servicio para el empleado

---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,10 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Employee } from '../models/employee.model';
+
+@Injectable({ providedIn: 'root' })
+export class EmployeeService {
+  private apiUrl = 'http://localhost:5001/api/employees';
+
+  constructor(private http: HttpClient) {}
+}
