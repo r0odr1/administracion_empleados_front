baf25b7ece881e3e761bc283beb194d31ecddc75
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-03-20 19:39:21 -0500

    Creacion de guard para proteger rutas

---
+++ src/app/guards/auth-guard.ts
@@ -0,0 +1,13 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const auth = inject(AuthService);
+
+  if (auth.isLoggedIn()) return true;
+
+  inject(Router).navigate(['/login']);
+
+  return false;
+};
