d6c87554a13d1e1c79ff2574f1e6c8e7270b3ed3
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-03-20 20:09:30 -0500

    Creacion componente para el login

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './login.component.html',
+})
+export class LoginComponent {
+
+  username = '';
+  password = '';
+  error = '';
+
+  constructor(private auth: AuthService, private router: Router) {}
+
+  onLogin() {
+    this.auth.login(this.username, this.password).subscribe({
+      next: () => this.router.navigate(['/employees']),
+      error: () => this.error = 'Credenciales inválidas'
+    });
+  }
+}
