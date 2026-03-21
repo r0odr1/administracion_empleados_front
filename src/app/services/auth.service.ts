dd630b15081473d0a6cfe3672d86359935a27646
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-03-20 20:18:15 -0500

    Actualizacion puerto del api

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,7 +7,7 @@ import { tap } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:5001/api/auth';
+  private apiUrl = 'http://localhost:5269/api/auth';
 
   constructor(private http: HttpClient, private router: Router) {}
 
