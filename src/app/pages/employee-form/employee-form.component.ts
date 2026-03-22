63ce974a5e9ed67f9317e1bb8ab4f4ac304728bd
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-03-22 13:55:41 -0500

    Actualizar funcionalidad del boton cancelar

--- src/app/pages/employee-form/employee-form.component.ts
+++ src/app/pages/employee-form/employee-form.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { EmployeeService } from '../../services/employee.service';
 import { Employee } from '../../models/employee.model';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employee-form',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './employee-form.component.html',
 })
 export class EmployeeFormComponent implements OnInit {
