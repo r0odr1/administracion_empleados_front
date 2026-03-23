4f4a5f67c6934e5f90f3b4da1a33f438fcfdc206
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-03-22 21:58:15 -0500

    Actualizando modelo para separar responsabilidades entre el modelo y el formulario

--- src/app/models/employee.model.ts
+++ src/app/models/employee.model.ts
@@ -4,6 +4,16 @@
   apellido: string;
   email: string;
   cargo: string;
-  fechaIngreso: string;
+  fechaIngreso: Date;
   estado: boolean;
 }
+
+export type EmployeeFormModel = {
+  id?: number;
+  nombre: string;
+  apellido: string;
+  email: string;
+  cargo: string;
+  fechaIngreso: string;
+  estado: boolean;
+};
